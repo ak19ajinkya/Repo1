bb6d035191791491e3c6fec655a905f7410fd64e
ak19ajinkya <ak19ajinkya@gmail.com>
2018-12-05 16:38:36 +0530

    change in b local side

--- src/git/package1/B.java
+++ src/git/package1/B.java
@@ -2,6 +2,7 @@ package git.package1;
 //local change1
 //repo change 1
 //repo change 2 local change local commit
+//local change in b 4
 public class B extends A{
 	public static void main(String[] args) {
 		
